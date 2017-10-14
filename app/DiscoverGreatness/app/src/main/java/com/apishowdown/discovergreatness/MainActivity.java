ba6ff8dd49557378a74016de059a01ac3ecbc787
Hajin Lee <hajinlee3@gmail.com>
2017-10-14 14:23:20 -0500

    Add about dialog. Add card.io SDK and card scanning capability.

--- app/DiscoverGreatness/app/src/main/java/com/apishowdown/discovergreatness/MainActivity.java
+++ app/DiscoverGreatness/app/src/main/java/com/apishowdown/discovergreatness/MainActivity.java
@@ -1,13 +1,67 @@
 package com.apishowdown.discovergreatness;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import io.card.payment.CardIOActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int CREDIT_CARD_SCAN_REQUEST_CODE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.aboutButton:
+                new AlertDialog.Builder(this).setTitle(R.string.about_this_app)
+                    .setView(R.layout.dialog_about)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
+                return true;
+            case R.id.addCardButton:
+                Intent scanIntent = new Intent(this, CardIOActivity.class);
+                scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: false
+                scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false); // default: false
+                scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false); // default: false
+                startActivityForResult(scanIntent, CREDIT_CARD_SCAN_REQUEST_CODE);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CREDIT_CARD_SCAN_REQUEST_CODE) {
+            if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+                // Use mock data
+                // TODO
+            }
+        }
     }
 }
