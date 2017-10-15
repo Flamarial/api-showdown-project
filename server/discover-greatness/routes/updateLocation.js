2255e984c8523b462151a38a19aee96c52fe03d9
ggustilo <beastifer@gmail.com>
2017-10-14 20:23:16 -0500

    Able to update location via route

---
+++ server/discover-greatness/routes/updateLocation.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const updateLocation = require('../lib/updateLocation');
+
+/* GET users listing. */
+router.post('/updateLocation', function(req, res, next) {
+    let body = req.body;
+    console.log(body);
+    updateLocation(body.latitude, body.longitude, body.wallet_id);
+    res.send().status(200);
+});
+
+module.exports = router;
