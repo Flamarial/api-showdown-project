088770aa58cd306d1f252dc9dbd9bf903ec2879d
ggustilo <beastifer@gmail.com>
2017-10-15 00:06:35 -0500

    basics for push notifications

---
+++ server/discover-greatness/lib/sendPushNotifications.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const config = require('config');
+const gcm = require('node-gcm');
+const serverKey = config.get("pushServerKey");
+
+// Set up the sender with your GCM/FCM API key (declare this once for multiple messages)
+const sender = new gcm.Sender('456321697');
+
+// Prepare a message to be sent
+let message = new gcm.Message({
+   data: { key1: 'msg1' }
+});
+
+// Specify which registration IDs to deliver the message to
+const regTokens = ['YOUR_REG_TOKEN_HERE'];
+
+// Actually send the message
+sender.send(message, { registrationTokens: regTokens }, function (err, response) {
+   if (err) console.error(err);
+   else console.log(response);
+});
