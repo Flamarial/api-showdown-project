040467fc041bb5c0f70233b43b540a5b38d117b8
Scott <iscott.chou@gmail.com>
2017-10-15 09:59:46 -0500

    Branch for fixing

--- server/discover-greatness/routes/registrationToken.js
+++ server/discover-greatness/routes/registrationToken.js
@@ -2,17 +2,13 @@
 
 const express = require('express');
 const router = express.Router();
-const updateLocation = require('../lib/sendPushNotifications');
+const sendPushNotifications = require('../lib/sendPushNotifications');
 
 /* GET users listing. */
 router.post('/registrationToken', function(req, res, next) {
     let body = req.body;
-    console.log(body.gcm_registration_token);
-<<<<<<< Updated upstream
-    res.status(200).send({"error": "Registration was not successful hajin. :("});
-=======
+    sendPushNotifications(body.gcm_registration_token);
     res.status(200).send();
->>>>>>> Stashed changes
 });
 
 module.exports = router;
