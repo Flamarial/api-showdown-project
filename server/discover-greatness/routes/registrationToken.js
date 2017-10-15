2e7e776f74a541722231e047466c613594e58bbe
Kevin Mark <kmark1625@gmail.com>
2017-10-15 01:40:31 -0700

    Updates

--- server/discover-greatness/routes/registrationToken.js
+++ server/discover-greatness/routes/registrationToken.js
@@ -8,7 +8,7 @@ const updateLocation = require('../lib/sendPushNotifications');
 router.post('/registrationToken', function(req, res, next) {
     let body = req.body;
     console.log(body.gcm_registration_token);
-    res.send().status(404);
+    res.send().status(404).send({"hello": "world"});
 });
 
 module.exports = router;
