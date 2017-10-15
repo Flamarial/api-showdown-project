158ed0d012e98ecafc86b27c892ea478b31f4102
Kevin Mark <kmark1625@gmail.com>
2017-10-15 08:13:16 -0700

    add log for reg token

--- server/discover-greatness/routes/registrationToken.js
+++ server/discover-greatness/routes/registrationToken.js
@@ -7,6 +7,7 @@ const updateLocation = require('../lib/sendPushNotifications');
 /* GET users listing. */
 router.post('/registrationToken', function(req, res, next) {
     let body = req.body;
+    console.log('registration token');
     console.log(body.gcm_registration_token);
     res.status(200).send({"error": "Registration was not successful hajin. :("});
 });
