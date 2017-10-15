d1ca6dec7bacf9f02fce41fc55c7c9d4dcfbe27b
ggustilo <beastifer@gmail.com>
2017-10-15 10:49:38 -0500

    Remove test mode from push notifications

--- server/discover-greatness/lib/sendPushNotifications.js
+++ server/discover-greatness/lib/sendPushNotifications.js
@@ -4,28 +4,25 @@ const config = require('config');
 const gcm = require('node-gcm');
 
 const sendPushNotifications = function(registrationToken){
-//   const serverKey = config.get("pushServerKey");
   // Set up the sender with your GCM/FCM API key (declare this once for multiple messages)
   const sender = new gcm.Sender("AAAAABsy6qE:APA91bEy0uEDjKw5UKSsY159CHnJbhIvDPWejuWV5hxBMMbKnt8VwLAi75WPf3F9tiveh384UMsvLELJmAWwlQYUL8BeVkTaYE62ulxEWU1HTc_eLF-gM2yShH0zZHhfniR5KIcXqpFH");
   // Prepare a message to be sent
-  // let message = new gcm.Message({
-  //    data: { key1: 'msg1' }
-  // });
+
   var message = new gcm.Message({
       collapseKey: 'demo',
       priority: 'high',
       contentAvailable: true,
       delayWhileIdle: true,
       timeToLive: 3,
-      dryRun: true,
+      dryRun: false,
       data: {
           key1: 'message1',
           key2: 'message2'
       },
       notification: {
-          title: "Hello, World",
+          title: "New Rewards",
           icon: "ic_launcher",
-          body: "This is a notification that will be displayed if your app is in the background."
+          body: "Your friend recently bought coffee and now you get %20 off!"
       }
   });
   message.addData({
@@ -35,10 +32,15 @@ const sendPushNotifications = function(registrationToken){
   // Specify which registration IDs to deliver the message to
   const regTokens = [];
   regTokens.push(registrationToken);
+
   sender.send(message, { registrationTokens: regTokens }, function(err, response) {
-    if(err) console.error(err);
-    else    console.log(response);
+        if(err) {
+            console.error(err);
+        } else {
+            console.log(response);
+        }
   });
+
 }
 module.exports = sendPushNotifications;
 
