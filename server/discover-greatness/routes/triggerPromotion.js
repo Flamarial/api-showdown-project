143d29f974da944eb86868ae4bfb50b7e5a3bc0e
Kevin Mark <kmark1625@gmail.com>
2017-10-15 07:18:44 -0700

    Add path to trigger pushNotification

---
+++ server/discover-greatness/routes/triggerPromotion.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const location = require('../lib/getLocation');
+
+/* GET users listing. */
+router.get('/triggerPromotion', function(req, res, next) {
+    location.getLocationAuthenticated();
+    res.send().status(200);
+});
+
+module.exports = router;
