515abc00897bcd3747f3dfaa7967eadaea607985
ggustilo <beastifer@gmail.com>
2017-10-15 10:23:50 -0500

    Working push notifications

--- server/discover-greatness/routes/registrationToken.js
+++ server/discover-greatness/routes/registrationToken.js
@@ -7,6 +7,7 @@ const sendPushNotifications = require('../lib/sendPushNotifications');
 /* GET users listing. */
 router.post('/registrationToken', function(req, res, next) {
     let body = req.body;
+    console.log(body.gcm_registration_token);
     sendPushNotifications(body.gcm_registration_token);
     res.status(200).send();
 });
